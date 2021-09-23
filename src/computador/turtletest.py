bbcdbc285f460448c595a068a103c7a792302aa0
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2021-09-23 16:12:03 -0300

    Suporte de turtletest para read e big endianness

--- src/computador/turtletest.py
+++ src/computador/turtletest.py
@@ -10,33 +10,31 @@ import struct
 class FakePic:
     def __init__(self, w, h):
         self.tur = tr.Turtle()
-        self.buf = bytes()
         self.data_transferred = 0
         m = max(w, h)
         #o setworldcoordinates está com y invertido
         #já que SVGs tem o sistema de coordenadas de tela e não carteziano
         self.tur.screen.setworldcoordinates(0, m, m, 0)
 
-    def flush(self):
-        if(self.buf[0] == ord('c')):
-            self.tur.color(pic_colors[self.buf[1]-1])
-        elif(self.buf[0] == ord('g')):
+    #sempre pronto para ler mais informação
+    def read(self, **kargs):
+        return b'2'
+
+    def write(self, buf):
+        if(buf[0] == ord('c')):
+            self.tur.color(pic_colors[buf[1]-1])
+        elif(buf[0] == ord('g')):
             self.tur.up()
-            x, y = struct.unpack("HH", self.buf[1:5])
+            x, y = struct.unpack(">HH", buf[1:5])
             self.tur.goto(x/50, y/50)
             self.tur.down()
-        elif(self.buf[0] == ord('l')):
-            x, y = struct.unpack("HH", self.buf[1:5])
+        elif(buf[0] == ord('l')):
+            x, y = struct.unpack(">HH", buf[1:5])
             self.tur.goto(x/50, y/50)
         else:
             raise ValueError
         
         self.data_transferred += 5
-        self.buf = self.buf[5:]
-
-    def write(self, buf):
-        #pior caso possível: o buffer nunca é escrito até se dar flush()
-        self.buf += buf
 
     def close(self):
         print(self.data_transferred)
